63cea7cbb9f08151ecf43e15a6f15263368d7176
TR4Q <jlchin@berkeley.edu>
2017-09-15 00:23:14 -0700

    Implements scraping of player_ids.

--- scraper/nba_response.py
+++ scraper/nba_response.py
@@ -2,10 +2,25 @@
 Defines a class representing a json response from stats.nba.com.
 """
 
-from typing import Dict, Any
+from typing import Dict
+
+headers_access = lambda json:json['resultSets'][0]['headers']
+row_set_access = lambda json:json['resultSets'][0]['rowSet']
 
 class NBA_response():
 
-    def __init__(self, json_response: Dict[str: Any]):
-        self.headers = []
-        self.rows = []
+    def __init__(self, json_response: Dict):
+        # corresponding to how NBA json responses are formatted
+        self._headers = headers_access(json_response)
+        self._rows = row_set_access(json_response)
+
+    @property
+    def headers(self):
+        return self._headers
+
+    @property
+    def rows(self):
+        return self._rows
+
+    def __str__(self):
+        return '{} rows with headers: {}'.format(len(self.rows), self.headers)
---
+++ scraper/scraper_config.py
@@ -0,0 +1,18 @@
+START_YEAR = 2010
+END_YEAR = 2017
+
+def _get_seasons():
+    """
+    Returns a list of strings representing seasons in the format:
+    '20XX-YY'
+    """
+    start_year = START_YEAR
+    end_year = END_YEAR
+
+    seasons = []
+    while start_year != end_year - 1:
+        seasons.append('{}-{}'.format(start_year, (start_year + 1) % 100))
+        start_year += 1
+    return seasons
+
+SEASONS = _get_seasons()
