f5ace766aedfd161d49a081689a9d14eb9ba2b54
Jason Chin <jsonchin@users.noreply.github.com>
2017-09-16 18:19:49 -0700

    Adds unittests. (#8)
    * Adds unit tests for db_storage.
    
    * Adds skeleton for unit tests for scraper.
    
    * Updates unittest to check if table was created.

---
+++ test/db_storage/test_db_storage.py
@@ -0,0 +1,33 @@
+import unittest
+from db_storage import db_config
+db_config.DB_NAME = 'test_db'
+db_config.DB_PATH = 'test/db_storage/databases'
+
+from db_storage import db_init
+from db_storage import db_storage
+
+
+class TestDBStorage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    @classmethod
+    def setUp(cls):
+        pass
+
+    def test_db_init(self):
+        """
+        Tests if init_db creates a table called 'scrape_log'.
+        """
+        db_init.init_db()
+        self.assertTrue(db_storage.exists_table('scrape_log'), 'Table \'scrape_log\' should exist.')
+
+    def test_exists_db(self):
+        self.assertFalse(db_storage.exists_table('TABLE_NAME_THAT_DOES_NOT_EXIST'),
+                         'Table should not exist.')
---
+++ test/scraper/test_scraper.py
@@ -0,0 +1,19 @@
+import unittest
+from db_storage import db_config
+db_config.DB_NAME = 'test_db'
+db_config.DB_PATH = 'test/db_storage/databases'
+
+
+class TestScraper(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    @classmethod
+    def setUp(cls):
+        pass
