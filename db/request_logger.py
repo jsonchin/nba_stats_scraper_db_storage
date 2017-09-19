66b42a2ab1036649597a14fc94df98df87e8b473
Jason Chin <jlchin@berkeley.edu>
2017-09-19 00:22:55 -0700

    Implements skipping of API requests that have been scraped. (#20)
    * Adds more API request jobs that are helpful.
    
    * Changes lowercase to uppercase for fillables.
    
    * Implements already_scraped.
    
    * Adds documentation for request_logger.
    
    * Separates already_scraped into two lines for readability.
    
    * Adds documentation for db.utils.
    
    * Changes default value of empty list to tuple.

--- db/request_logger.py
+++ db/request_logger.py
@@ -3,5 +3,18 @@ import db.utils
 
 
 def log_request(api_request):
+    """
+    Logs the api_request with a time stamp to the table
+    called "scrape_log".
+    """
     curr_time = datetime.now().strftime('%Y-%m-%d %X %f')
     db.utils.execute_sql("""INSERT INTO scrape_log VALUES (?, ?);""", (curr_time, api_request))
+
+def already_scraped(api_request):
+    """
+    Returns True if the api_request has already been scraped.
+    This is determined by whether or not the api_request str
+    exists within the table "scrape_log.
+    """
+    api_request_log = db.utils.execute_sql("""SELECT * FROM scrape_log WHERE api_request = ? LIMIT 1;""", (api_request, ))
+    return len(api_request_log) != 0
--- db/utils.py
+++ db/utils.py
@@ -3,12 +3,20 @@ import db.config as DB_CONFIG
 
 con = sqlite3.connect('{}/{}.db'.format(DB_CONFIG.DB_PATH, DB_CONFIG.DB_NAME))
 
-def execute_sql(sql, params=[]):
+def execute_sql(sql, params=()):
+    """
+    Executes a sql query and commits the result.
+    params is a list of values that will be used
+    in place of question marks in the sql statement.
+    """
     cur = con.execute(sql, params)
     con.commit()
     return cur.fetchall()
 
 def execute_many_sql(sql, seq_of_params):
+    """
+    Executes a sql statement for a batch of values.
+    """
     cur = con.executemany(sql, seq_of_params)
     con.commit()
     return cur.fetchall()
