7ec1086524ba59ea59039713f92a321465b030c3
TR4Q <jlchin@berkeley.edu>
2017-09-28 23:46:06 -0700

    Implements minimal (non-redundant) scraping.

--- db/request_logger.py
+++ db/request_logger.py
@@ -18,3 +18,13 @@ def already_scraped(api_request):
     """
     api_request_log = db.utils.execute_sql("""SELECT * FROM scrape_log WHERE api_request = ? LIMIT 1;""", (api_request, ))
     return len(api_request_log) != 0
+
+
+def get_last_scraped(api_request):
+    """
+    Returns True if the api_request has already been scraped.
+    This is determined by whether or not the api_request str
+    exists within the table "scrape_log.
+    """
+    api_request_date_query = db.utils.execute_sql("""SELECT MAX(date) FROM scrape_log WHERE api_request = ?;""", (api_request, ))
+    return api_request_date_query[0][0]
