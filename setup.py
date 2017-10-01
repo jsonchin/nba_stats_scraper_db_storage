3e0c2afa50cd42b9f147ad2f1de0dee2bc1631da
TR4Q <jlchin@berkeley.edu>
2017-10-01 11:27:55 -0700

    Adds a description of init_db and api_requests_init.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     install_requires=required
 )
 
-
+# initialize the database and run the initial scrapes that
+# are necessary for this tool (player_ids and game_dates)
 db.initialize.init_db()
 scrape.scraper.run_scrape_jobs('scrape/api_requests_init.yaml')
