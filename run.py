fb9269f0ef03ea28eb675d6bd45585881598c6d4
TR4Q <jlchin@berkeley.edu>
2017-10-08 21:14:06 -0700

    Adds delete scrape_log entries before date cmd line arg.

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 import argparse
 import scrape.scraper
+import db.utils
 
 parser = argparse.ArgumentParser(description='NBA Stats Scraper and Storage')
 parser.add_argument('--scrape', nargs='?', const='api_requests.yaml')
+parser.add_argument('--delete', nargs='?', const=None,
+                    help="""Deletes all entries in scrape_log before the supplied date. If no date is supplied all entries are removed.""")
 
 args = parser.parse_args()
 
 if args.scrape is not None:
     scrape.scraper.run_scrape_jobs(args.scrape)
+
+if args.delete is not None:
+    db.utils.reset_scrape_logs(args.delete)
