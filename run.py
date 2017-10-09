5bb161eabb2f8dd8e3d911eee63ffd5d117f37cd
TR4Q <jlchin@berkeley.edu>
2017-10-08 20:51:10 -0700

    Adds command line interface for yaml location.

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import argparse
 import scrape.scraper
-import db.utils
 
+parser = argparse.ArgumentParser(description='NBA Stats Scraper and Storage')
+parser.add_argument('--scrape', nargs='?', const='api_requests.yaml')
 
-if __name__ == '__main__':
-    scrape.scraper.run_scrape_jobs('api_requests.yaml')
+args = parser.parse_args()
+
+if args.scrape is not None:
+    scrape.scraper.run_scrape_jobs(args.scrape)
